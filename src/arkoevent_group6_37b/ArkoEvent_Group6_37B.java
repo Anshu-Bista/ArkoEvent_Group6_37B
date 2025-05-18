f94f3b852555199acd6ca5772bef534a3d3a17c5
Dawn <bistanshu3@gmail.com>
2025-05-18 16:28:40 +0545

    folder created

---
+++ src/arkoevent_group6_37b/ArkoEvent_Group6_37B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arkoevent_group6_37b;
+
+/**
+ *
+ * @author hp
+ */
+public class ArkoEvent_Group6_37B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
