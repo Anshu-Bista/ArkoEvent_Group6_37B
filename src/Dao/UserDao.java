285827548ddbf6aaebab8478728c0c3060b184bf
Prashant Shrestha <prashantshrestha206@gmail.com>
2025-05-23 19:37:17 +0545

    login

---
+++ src/Dao/UserDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+import Database.MySqlConnection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.UserData;
+
+
+/**
+ *
+ * @author asus
+ */
+public class UserDao {
+    
+    
+}
