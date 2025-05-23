073d238ec80a26b1683d5e6be4eea70893246041
Dawn <bistanshu3@gmail.com>
2025-05-23 17:34:16 +0545

    reset password dao

--- src/Dao/UserDao.java
+++ src/Dao/UserDao.java
@@ -8,8 +8,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.time.ZoneId;
+
 
 import Database.MySqlConnection;
 import Model.UserData;
@@ -22,12 +25,13 @@ public class UserDao {
     MySqlConnection mysql = new MySqlConnection();
     Connection conn = mysql.openConnection();
 
+    //Sign Up
     public void signUp(UserData user){
         String sql = "INSERT into users(username, email, password) values(?, ?, ?)";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)){
             pstmt.setString(1, user.getUsername());
-            pstmt.setString(1, user.getEmail());
-            pstmt.setString(1, user.getPassword());  
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPassword());  
         }
         catch(SQLException ex){
             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE,null, ex);
@@ -36,7 +40,8 @@ public class UserDao {
             mysql.closeConnection(conn);          
         }
     }
-    
+
+    //Checks if the user is present
     public boolean checkUser(UserData user){
         Connection conn = mysql.openConnection();
         String sql ="SELECT* FROM users where email = ? or username = ?";
@@ -55,6 +60,86 @@ public class UserDao {
         return false;
     }
 
+    //Forgot Password
+    //check if email is in the table
+    public boolean checkEmail(UserData user){
+        Connection conn = mysql.openConnection();
+        String sql ="SELECT id FROM users where email = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, user.getEmail());
+            ResultSet result = pstmt.executeQuery();
+            return result.next();
+        }
+        catch(SQLException ex){
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
 
-    
+    //save reset code and current timestamp 
+    public void saveResetCode(String email, String resetCode){
+        Connection conn = mysql.openConnection();
+        String sql ="UPDATE users SET reset_code = ?, reset_code_timestamp = NOW() WHERE email = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, resetCode);
+            pstmt.setString(2, email);          
+            pstmt.executeUpdate();
+        }
+        catch(SQLException ex){
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            mysql.closeConnection(conn);
+        }
+    }
+
+    //verify the code and time 
+    public boolean verifyResetCode(String email, String resetCode){
+        Connection conn = mysql.openConnection();
+        String sql ="SELECT reset_code_timestamp FROM users WHERE email= ? AND reset_code = ? ";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, email);
+            pstmt.setString(2, resetCode);          
+            ResultSet result = pstmt.executeQuery();
+
+            if(result.next()){
+                LocalDateTime generatedAt = result.getTimestamp("reset_code_timestamp").toLocalDateTime();
+                LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault());
+                //valid only if within 3 minutes
+                if (generatedAt.plusMinutes(3).isAfter(now)){
+                    return true;
+                }
+            }
+        }
+        catch(SQLException ex){
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
+
+    //update password and clear reset code nad timestamp
+    public boolean updatePassword(String email, String newPassword){
+        Connection conn = mysql.openConnection();
+        String sql ="UPDATE users SET password = ?, reset_code = null, reset_code_timestamp = null WHERE email = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, newPassword);
+            pstmt.setString(2, email);          
+            int rowsUpdated  = pstmt.executeUpdate();
+            return rowsUpdated > 0; // success if atleast one row was updated
+
+        }
+        catch(SQLException ex){
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
 }
