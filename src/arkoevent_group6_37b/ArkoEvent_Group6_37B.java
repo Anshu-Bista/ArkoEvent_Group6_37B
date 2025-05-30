fd3e2d732eb6a1e2c65d319037e84b35c96047f7
Prasanna Shrestha <prasannasht7@gmail.com>
2025-05-30 18:36:33 +0545

    Add files via upload

--- src/arkoevent_group6_37b/ArkoEvent_Group6_37B.java
+++ src/arkoevent_group6_37b/ArkoEvent_Group6_37B.java
@@ -4,17 +4,36 @@
  */
 package arkoevent_group6_37b;
 
+import Database.MySqlConnection;
+import Database.*;
+
 /**
  *
  * @author hp
  */
 public class ArkoEvent_Group6_37B {
-
+    public static void main(String[]args){
+        Database db = new MySqlConnection();
+        System.out.println(db);
+        try {
+            db.openConnection();
+            System.out.println(db.openConnection());
+//            if (db.openConnection() != null){
+//                System.out.println("Database  connected successfully!");
+//               }else{
+//                System.out.println("Failed to connect to database.");
+//            }
+        } catch(Exception e){
+           System.out.println("Failed to connect to database.");
+           System.out.println(e);
+       }
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+  
         // TODO code application logic here
     }
     
-}
+
