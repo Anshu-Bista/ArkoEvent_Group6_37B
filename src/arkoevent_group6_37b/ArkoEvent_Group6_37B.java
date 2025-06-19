97445f37f006e20083a4a72aef5c014105868916
Ekta879 <ekatathapa13@gmail.com>
2025-06-19 19:34:30 +0545

    Add files via upload

--- src/arkoevent_group6_37b/ArkoEvent_Group6_37B.java
+++ src/arkoevent_group6_37b/ArkoEvent_Group6_37B.java
@@ -1,21 +1,13 @@
 package arkoevent_group6_37b;
 
 import View.Reset;
-import controller.PasswordUpdateController;
 
 public class ArkoEvent_Group6_37B {
 
     public static void main(String[] args) {
-        // Example username - this would typically come from a login screen
-        String username = "testUser";
-
-        // Create the Reset view (UI)
-        Reset resetView = new Reset(username);
-
-        // Connect the controller (optional - depends on your structure)
-        PasswordUpdateController controller = new PasswordUpdateController(resetView);
-
-        // Open the UI window
-        controller.open();
+    Reset resetView = new Reset(); // No parameter
+        resetView.setVisible(true);
     }
 }
+
+
